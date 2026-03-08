9e0351d471f12a474e1d1e95214d12ae02ccab41
user <user@DESKTOP-IUIGGTS>
2026-03-08 15:58:52 +0200

    aa

--- src/main/java/admain/database_connection.java
+++ src/main/java/admain/database_connection.java
@@ -9,9 +9,9 @@ import javax.swing.JOptionPane;
 public class database_connection {
 
 	 
-    private static final String URL = "jdbc:postgresql://localhost:5432/Appointment_Scheduling_System";
-   private static final String USER = "postgres";
-   private static final String PASSWORD = "your_password";
+    private static final String URL = "jdbc:postgresql://localhost:5432/Appointment";
+   private static final String USER = "Appointment_Scheduling_System";
+   private static final String PASSWORD = "123456";
 
    private static Connection connection;
 
