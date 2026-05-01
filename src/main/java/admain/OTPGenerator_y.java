ccf8036c10b44e149f342dfa169a359243d73907
yasmeen <s12323814@stu.najah.edu>
2026-05-01 19:57:18 +0300

    Update OTPGenerator_y.java

--- src/main/java/admain/OTPGenerator_y.java
+++ src/main/java/admain/OTPGenerator_y.java
@@ -3,9 +3,10 @@ package admain;
 import java.util.Random;
 
 public class OTPGenerator_y {
+  private static final Random rand = new Random();
 
     public static String generateOTP() {
-        Random rand = new Random();
+    
         int otp = 100000 + rand.nextInt(900000);
         return String.valueOf(otp);
     }
