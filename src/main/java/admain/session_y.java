354d97a1cf9269a04ab228257bb2b010153ec9ff
لانا لؤي معروف زبادي <s12323241@stu.najah.edu>
2026-05-06 18:42:19 +0300

    Update session_y.java

--- src/main/java/admain/session_y.java
+++ src/main/java/admain/session_y.java
@@ -1,41 +1,42 @@
 package admain;
 
+import java.util.logging.Logger;
+
 public class session_y {
+
+    private static final Logger logger = Logger.getLogger(session_y.class.getName());
 
     private Account_y account;
 
     public static Account_y currentUser = null;
     public static Account_y currentAdmin = null;
 
- 
     public static void logoutUser() {
         if (currentUser != null) {
-            System.out.println("Logging out user: " + currentUser.getUsername());
+            logger.info("Logging out user: " + currentUser.getUsername());
             currentUser = null;
         } else {
-            System.out.println("No user is currently logged in.");
+            logger.warning("No user is currently logged in.");
         }
     }
 
-  
     public static void logoutAdmin() {
         if (currentAdmin != null) {
-            System.out.println("Logging out admin: " + currentAdmin.getUsername());
+            logger.info("Logging out admin: " + currentAdmin.getUsername());
             currentAdmin = null;
         } else {
-            System.out.println("No admin is currently logged in.");
+            logger.warning("No admin is currently logged in.");
         }
     }
+
     public session_y(Account_y account) {
         this.account = account;
     }
-
 
     public Account_y getAccount() {
         return account;
     }
 
-   
     public boolean isAdmin() {
         return account.getRole() == Role_y.ADMIN;
     }
@@ -43,5 +44,4 @@ public class session_y {
     public boolean isUser() {
         return account.getRole() == Role_y.USER;
     }
-   
 }
