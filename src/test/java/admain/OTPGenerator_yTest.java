e58b9900fa6d26cfc4a17e0d0ee2395c61f0749e
لانا لؤي معروف زبادي <s12323241@stu.najah.edu>
2026-05-09 00:13:39 +0300

    Update OTPGenerator_yTest.java

--- src/test/java/admain/OTPGenerator_yTest.java
+++ src/test/java/admain/OTPGenerator_yTest.java
@@ -1,19 +1,16 @@
 package admain;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class OTPGenerator_yTest {
+class OTPGeneratorTest {
 
     @Test
-    void testGenerateOTP() {
+    void testGenerateOTP_shouldReturn6DigitNumber() {
         String otp = OTPGenerator_y.generateOTP();
 
         assertNotNull(otp);
         assertEquals(6, otp.length());
-        assertTrue(otp.matches("\\d{6}"));
-
         int value = Integer.parseInt(otp);
         assertTrue(value >= 100000 && value <= 999999);
     }
