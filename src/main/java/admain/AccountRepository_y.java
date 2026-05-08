f033adce067492cac7d6757cfe28686a8abca5d0
لانا لؤي معروف زبادي <s12323241@stu.najah.edu>
2026-05-08 18:57:46 +0300

    Update AccountRepository_y.java

--- src/main/java/admain/AccountRepository_y.java
+++ src/main/java/admain/AccountRepository_y.java
@@ -6,10 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
-public class AccountRepository_y {
+public class AccountRepository {
 
     private static final Logger logger =
-            Logger.getLogger(AccountRepository_y.class.getName());
+            Logger.getLogger(AccountRepository.class.getName());
 
     public Account_y findByUsernameOrEmail(String input) {
 
