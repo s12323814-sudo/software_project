53a46cb4b8cad037f024e4e5d7f0841dc20bbfa9
لانا لؤي معروف زبادي <s12323241@stu.najah.edu>
2026-05-08 22:09:52 +0300

    Update OTPGenerator_yTest.java

--- src/test/java/admain/OTPGenerator_yTest.java
+++ src/test/java/admain/OTPGenerator_yTest.java
@@ -8,33 +8,28 @@ class OTPGenerator_yTest {
 
     @Test
     void testGenerateOTP_notNull() {
-
         String otp = OTPGenerator_y.generateOTP();
-
         assertNotNull(otp);
     }
 
     @Test
     void testGenerateOTP_lengthIs6() {
-
         String otp = OTPGenerator_y.generateOTP();
-
         assertEquals(6, otp.length());
     }
 
     @Test
     void testGenerateOTP_isNumeric() {
-
         String otp = OTPGenerator_y.generateOTP();
-
         assertTrue(otp.matches("\\d{6}"));
     }
 
     @Test
     void testGenerateOTP_range() {
+        String otp = OTPGenerator_y.generateOTP();
 
-        int otp = Integer.parseInt(OTPGenerator_y.generateOTP());
+        int value = Integer.parseInt(otp);
 
-        assertTrue(otp >= 100000 && otp <= 999999);
+        assertTrue(value >= 100000 && value <= 999999);
     }
 }
